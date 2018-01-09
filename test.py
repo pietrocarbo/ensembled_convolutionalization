3f1df33799ef0ed44a503d78ed6e7431272be9d7
Pietro Battilana <battilanap@gmail.com>
2018-01-09 11:44:24 +0100

    FC->CONV

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Input, Conv2D
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Input, Conv2D, AveragePooling2D
 from keras.models import Model
 from keras.models import model_from_json
 import json
+import numpy as np
+import os
+
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing import image
 
 def prepare_str_file_architecture_syntax(filepath):
     model_str = str(json.load(open(filepath, "r")))
@@ -16,7 +21,6 @@ model.load_weights("2017-12-24_acc77_vgg16/vgg16_ft_weights_acc0.78_e15_2017-12-
 print("IMPORTED MODEL")
 model.summary()
 
-
 p_dim = model.get_layer("global_average_pooling2d_1").input_shape  # None,7,7,512
 out_dim = model.get_layer("output_layer").get_weights()[1].shape[0]  # None,101
 W, b = model.get_layer("output_layer").get_weights()
@@ -28,22 +32,36 @@ print("weights new shape", weights_shape)
 
 new_W = W.reshape(weights_shape)
 
-global_pool_layer = model.get_layer("global_average_pooling2d_1")
-global_pool_layer.outbound_nodes = []
+last_pool_layer = model.get_layer("block5_pool")
+last_pool_layer.outbound_nodes = []
+model.layers.pop()
 model.layers.pop()
 
 for i, l in enumerate(model.layers):
     print(i, ":", l.name)
 
-model.layers[19].output_shape
+x = AveragePooling2D(pool_size=(7, 7))(last_pool_layer.output)
 
-pool_output = global_pool_layer.output
+x = Conv2D(101, (1, 1), strides=(1, 1), activation='relu', padding='valid', weights=[new_W, b])(x)
 
-output = Conv2D(out_dim, p_dim[3], strides=1, activation='relu', padding='valid', weights=[new_W, b], input_shape=global_pool_layer.output_shape)(pool_output)
-# error here!
-print("conv2d attached to global pooling")
-
-model = Model(inputs=model.get_layer("input_1").input, outputs=output)
+model = Model(inputs=model.get_layer("input_1").input, outputs=x)
 
 print("CONVOLUTIONALIZATED MODEL")
 model.summary()
+
+
+input_file = "test_images/pizza.jpg"
+img = image.load_img(input_file)  # , target_size=(224, 224)
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+preds = model.predict(x)
+
+preds = preds.flatten()
+sorted_classes_ix = np.argsort(preds).flatten()
+result = 'Image ' + str(input_file) + ' results:\n'
+with open(os.path.join('dataset-ethz101food', 'meta', 'classes.txt')) as file:
+    class_labels = [line.strip('\n') for line in file.readlines()]
+for i in range(5):
+    idx = sorted_classes_ix[-i]
+    result += '\t prediction {:d}/{:d}  -->  classified as: {:s}({:d}) with a confidence of {:f}\n'.format(i + 1, 5, class_labels[idx], idx, preds[idx])
